af3a96ef978aced0800ac97cc7b91a5b3dc486c9
tosico85 <tosico85@gmail.com>
2023-09-19 21:30:43 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,88 +31,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: Uri.parse('https://first-drive-jwt.vercel.app/login'),
-            ),
-            initialOptions: InAppWebViewGroupOptions(
-              android: AndroidInAppWebViewOptions(
-                allowContentAccess: true,
-                builtInZoomControls: true,
-                thirdPartyCookiesEnabled: true,
-                allowFileAccess: true,
-                supportMultipleWindows: true,
+    return WillPopScope(
+      // 뒤로 가기 버튼을 처리할 때 호출되는 함수
+      onWillPop: () async {
+        if (_webViewController != null &&
+            await _webViewController!.canGoBack()) {
+          // 웹뷰에서 뒤로 가기가 가능한 경우 웹뷰에서 페이지를 뒤로 이동
+          _webViewController!.goBack();
+          return false; // 뒤로 가기 이벤트 막음
+        }
+        return true; // 앱 종료 (뒤로 가기 이벤트 허용)
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: Uri.parse('https://first-drive-jwt.vercel.app/login'),
               ),
-              crossPlatform: InAppWebViewOptions(
-                verticalScrollBarEnabled: true,
-                clearCache: true,
-                disableContextMenu: false,
-                cacheEnabled: true,
-                javaScriptEnabled: true,
-                javaScriptCanOpenWindowsAutomatically: true,
-                transparentBackground: true,
-              ),
-            ),
-            onWebViewCreated: (InAppWebViewController controller) {
-              _webViewController = controller;
-            },
-            onCreateWindow: (controller, createWindowRequest) async {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return Dialog(
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: InAppWebView(
-                        // Setting the windowId property is important here!
-                        windowId: createWindowRequest.windowId,
-                        initialOptions: InAppWebViewGroupOptions(
-                          android: AndroidInAppWebViewOptions(
-                              builtInZoomControls: true,
-                              thirdPartyCookiesEnabled: true,
-                              useWideViewPort: true),
-                          crossPlatform: InAppWebViewOptions(
-                            cacheEnabled: true,
-                            javaScriptEnabled: true,
-                            userAgent:
-                                "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
-                          ),
-                        ),
-                        onWebViewCreated:
-                            (InAppWebViewController controller) {},
-                        onCloseWindow: (controller) {
-                          // On Facebook Login, this event is called twice,
-                          // so here we check if we already popped the alert dialog context
-                          if (Navigator.canPop(context)) {
-                            Navigator.pop(context);
-                          }
-                        },
-                      ),
-                    ),
-                  );
-                },
-              );
-              return true;
-            },
-          ),
-          for (var webView in _popupWebViews)
-            Positioned.fill(
-              child: InAppWebView(
-                initialUrlRequest: URLRequest(
-                  url: Uri.parse('about:blank'),
+              initialOptions: InAppWebViewGroupOptions(
+                android: AndroidInAppWebViewOptions(
+                  allowContentAccess: true,
+                  builtInZoomControls: true,
+                  thirdPartyCookiesEnabled: true,
+                  allowFileAccess: true,
+                  supportMultipleWindows: true,
                 ),
-                onWebViewCreated: (InAppWebViewController controller) {
-                  controller.loadUrl(
-                      urlRequest: URLRequest(url: Uri.parse('about:blank')));
-                  _webViewController = controller;
-                },
+                crossPlatform: InAppWebViewOptions(
+                  verticalScrollBarEnabled: true,
+                  clearCache: true,
+                  disableContextMenu: false,
+                  cacheEnabled: true,
+                  javaScriptEnabled: true,
+                  javaScriptCanOpenWindowsAutomatically: true,
+                  transparentBackground: true,
+                ),
               ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              onCreateWindow: (controller, createWindowRequest) async {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Dialog(
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: InAppWebView(
+                          // Setting the windowId property is important here!
+                          windowId: createWindowRequest.windowId,
+                          initialOptions: InAppWebViewGroupOptions(
+                            android: AndroidInAppWebViewOptions(
+                                builtInZoomControls: true,
+                                thirdPartyCookiesEnabled: true,
+                                useWideViewPort: true),
+                            crossPlatform: InAppWebViewOptions(
+                              cacheEnabled: true,
+                              javaScriptEnabled: true,
+                              userAgent:
+                                  "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
+                            ),
+                          ),
+                          onWebViewCreated:
+                              (InAppWebViewController controller) {},
+                          onCloseWindow: (controller) {
+                            if (Navigator.canPop(context)) {
+                              Navigator.pop(context);
+                            }
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                );
+                return true;
+              },
             ),
-        ],
+            for (var webView in _popupWebViews)
+              Positioned.fill(
+                child: InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse('about:blank'),
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    controller.loadUrl(
+                        urlRequest: URLRequest(url: Uri.parse('about:blank')));
+                    _webViewController = controller;
+                  },
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
